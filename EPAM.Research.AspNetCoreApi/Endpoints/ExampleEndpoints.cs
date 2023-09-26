e9035f3287797b21432f7a2966ebcbc3b3fb5873
ramboe <cle.kruse@hotmail.com>
2023-09-26 11:42:56 +0200

    introduced swagger

---
+++ EPAM.Research.AspNetCoreApi/Endpoints/ExampleEndpoints.cs
@@ -0,0 +1,25 @@
+﻿using Ramboe.MinimalApis;
+
+namespace EPAM.Research.AspNetCoreApi.Endpoints;
+
+public class ExampleEndpoints : IEndpoints
+    {
+        public static void DefineEndpoints(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/", () => "hello from example endpoints");
+            app.MapGet("/stuff", doStuff);
+        }
+
+        public static void AddServices(IServiceCollection services, IConfiguration configuration, bool isDev)
+        {
+            //services that these endpoints require
+        }
+
+        static IResult doStuff(HttpContext context)
+        {
+            var body = context.Request.Body.ToString();
+
+
+            return Results.Ok(body);
+        }
+    }
--- EPAM.Research.AspNetCoreApi/Program.cs
+++ EPAM.Research.AspNetCoreApi/Program.cs
@@ -1,6 +1,19 @@
+using Ramboe.MinimalApis;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//endpoints
+var isDev = builder.Environment.IsDevelopment();
+builder.Services.AddEndpoints<Program>(builder.Configuration, isDev);
+
+//swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseEndpoints<Program>();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
